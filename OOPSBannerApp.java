823bedbbfeb436b02a798e0b4c09350de35151df
Thanush02 <marupudithanush@gmail.com>
2026-03-03 10:49:51 +0530

    UC4: Refactor using String array and enhanced for-loop

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,46 +2,55 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        System.out.println(String.join("  ",
+        // Step 1: Create String array to hold banner lines
+        String[] lines = new String[7];
+
+        // Step 2: Populate array using String.join()
+        lines[0] = String.join("  ",
                 "  *****  ",
                 "  *****  ",
                 "  *****  ",
-                "  *****  "));
+                "  *****  ");
 
-        System.out.println(String.join("  ",
+        lines[1] = String.join("  ",
                 " *     * ",
                 " *     * ",
                 " *     * ",
-                " *        "));
+                " *        ");
 
-        System.out.println(String.join("  ",
+        lines[2] = String.join("  ",
                 " *     * ",
                 " *     * ",
                 " *     * ",
-                " *        "));
+                " *        ");
 
-        System.out.println(String.join("  ",
+        lines[3] = String.join("  ",
                 " *     * ",
                 " *     * ",
                 "  *****  ",
-                "  *****  "));
+                "  *****  ");
 
-        System.out.println(String.join("  ",
+        lines[4] = String.join("  ",
                 " *     * ",
                 " *     * ",
                 " *        ",
-                "        * "));
+                "        * ");
 
-        System.out.println(String.join("  ",
+        lines[5] = String.join("  ",
                 " *     * ",
                 " *     * ",
                 " *        ",
-                "        * "));
+                "        * ");
 
-        System.out.println(String.join("  ",
+        lines[6] = String.join("  ",
                 "  *****  ",
                 "  *****  ",
                 " *        ",
-                "  *****  "));
+                "  *****  ");
+
+        // Step 3: Use enhanced for-loop to print
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
